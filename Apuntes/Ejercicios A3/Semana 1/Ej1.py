37b141fd741b1779dd441a6c388a3096c8099ce4
Ricardo Eiras <ricardoeirasespineira@gmail.com>
2024-09-16 10:25:05 +0200

    Main

---
+++ Apuntes/Ejercicios A3/Semana 1/Ej1.py
@@ -0,0 +1,4 @@
+peso=float(input("Dame tu peso (kg): "))
+altura=float(input("Dame tu altura (m): "))
+IMC=peso/(altura*altura)
+print("Tu IMC es: " + str(IMC))
---
+++ Apuntes/Ejercicios A3/Semana 1/Ej2.py
@@ -0,0 +1,17 @@
+importe=int(input("Dame un importe en euros sin centimos: "))
+num=0
+a="============================================"
+
+print(a)
+print("Para tener "+str(importe)+" puedes tener esta cantidad de billetes:")
+
+print(str(num)+" billetes de 500€ \n")
+print(str(num)+" billetes de 200€ \n")
+print(str(num)+" billetes de 100€ \n")
+print(str(num)+" billetes de 50€ \n")
+print(str(num)+" billetes de 20€ \n")
+print(str(num)+" billetes de 10€ \n")
+print(str(num)+" billetes de 5€ \n")
+print(str(num)+" monedas de 2€ \n")
+print(str(num)+" billetes de 1€ \n")
+print(a)
